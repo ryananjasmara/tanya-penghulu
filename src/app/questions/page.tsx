6897f1050a6486e29fb058d03fa51fc35aeecc83
ryananjasmara <ryananjasmara1997@gmail.com>
2025-03-07 18:40:13 +0700

    feat: add question page

---
+++ src/app/questions/page.tsx
@@ -0,0 +1,9 @@
+import { SidebarClient } from "@/components/layout/Sidebar";
+
+export default function QuestionsPage() {
+  return (
+    <SidebarClient>
+      <div>Questions</div>
+    </SidebarClient>
+  );
+}
