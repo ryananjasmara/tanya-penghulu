713f74d88e7ac5632bcca226cf5cb62b2b8e82dd
ryananjasmara <ryananjasmara1997@gmail.com>
2025-03-07 18:31:03 +0700

    feat: add knowledge page

---
+++ src/app/knowledges/page.tsx
@@ -0,0 +1,9 @@
+import { SidebarClient } from "@/components/layout/Sidebar";
+
+export default function KnowledgesPage() {
+  return (
+    <SidebarClient>
+      <div>Knowledges</div>
+    </SidebarClient>
+  );
+}
