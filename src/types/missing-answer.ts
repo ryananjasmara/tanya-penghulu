2dce52132f11335835cce2fb636921eabde9fff9
ryananjasmara <ryananjasmara1997@gmail.com>
2025-03-10 12:42:05 +0700

    feat: add integration of missing-answers list

--- src/types/missing-answer.ts
+++ src/types/missing-answer.ts
@@ -12,5 +12,23 @@ export interface ICreateMissingAnswerResponse {
   status: boolean;
   message: string;
   data: IMissingAnswer;
+  error?: unknown;
 }
 
+export interface IGetAllMissingAnswersResponse {
+  status: boolean;
+  message: string;
+  data: IMissingAnswer[];
+  meta: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+  };
+  error?: unknown;
+}
+
+export interface IGetAllMissingAnswersParams {
+  page: number;
+  limit: number;
+}
