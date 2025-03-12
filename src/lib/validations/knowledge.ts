aa26d8a0d7e10df2493784577449a6bccdefbe5a
ryananjasmara <ryananjasmara1997@gmail.com>
2025-03-12 14:15:31 +0700

    fix: make category is mandatory in knowledge POST

--- src/lib/validations/knowledge.ts
+++ src/lib/validations/knowledge.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const KnowledgeSchema = z.object({
   keywords: z.array(z.string()).min(1),
   answer: z.string().min(10),
-  category: z.string().optional(),
+  category: z.string().min(1),
 });
